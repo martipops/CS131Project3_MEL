162a1fdebfc2efb8b6ace4b745484f707393ac5a
martipops <melonnemann@gmail.com>
2023-03-27 13:49:15 -0400

    first commit

---
+++ src/linkedlist/GenericLinkedList.java
@@ -0,0 +1,70 @@
+package linkedlist;
+
+/**
+ * This class models a linked list in which each node has a pointer
+ * to the node after it, except the tail which does not have a node after.
+ * 
+ * @author rkelley/njohnson
+ *         Programming Project 1 Start Project
+ *         CS131ON
+ */
+public class GenericLinkedList {
+
+    private GenericNode head; // node to represent the head of the list
+    private GenericNode tail; // node to represent the tail (end) of the list
+    private int length; // variable to keep track of the number of elements in the list
+
+    /**
+     * The constructor for this object initializes the list such that
+     * it is empty (no head or tail) node.
+     */
+    public GenericLinkedList() {
+        head = null;
+        length = 0;
+        tail = head;
+    }// end empty-argument constructor
+
+    /**
+     * Method to see if there are any elements in the list.
+     * 
+     * @return boolean true/false
+     */
+    public boolean isEmpty() {
+        return head == null;
+    }// end isEmpty
+
+    /**
+     * This method returns the head node of the list from which
+     * you can traverse the entire by following node links.
+     * 
+     * @return
+     */
+    public GenericNode getList() {
+        return head;
+    }// end getList
+
+    /**
+     * This method adds a new generic node to the list. It first checks to see
+     * if the list is empty. If so, it sets the head and the tail nodes
+     * to the node passed in. If the list is not empty it places the node
+     * at the end of the list and updates the pointers in the tail. This
+     * implementation does not support adding entries to the middle
+     * of the list.
+     * 
+     * @param aGenericNode a node that requires a parameterized type
+     */
+    public void addNode(GenericNode aGenericNode) {
+        if (isEmpty()) {
+            head = aGenericNode;
+            tail = head;
+            return;
+        }
+
+        tail.setNextNode(aGenericNode);
+        tail = tail.getNextNode();
+        tail.setNextNode(null);
+        length++;
+
+    }// end addNode
+
+}// end class
---
+++ src/linkedlist/GenericNode.java
@@ -0,0 +1,77 @@
+package linkedlist;
+
+/**
+ * 
+ * This class models a Node object which contains any type of object passed as a
+ * parameter. Each node has its internal data (of a certain type) and pointers
+ * to the next node in the list.
+ * 
+ * @param <T> The type of object this class is holding.
+ * 
+ * @Author Marti Lonnemann
+ */
+public class GenericNode<T> {
+
+    private T data; // data that the object stores
+    private GenericNode<T> nextNode; // pointer to the nextNode that will be in LinkedList
+
+    /**
+     * 
+     * Constructor build Node object. Initializes nextNode and data
+     * to null.
+     */
+    public GenericNode() {
+        setNextNode(null);
+        data = null;
+    }
+
+    /**
+     * 
+     * This method sets the data of the object.
+     * 
+     * @param data
+     */
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    /**
+     * 
+     * This method returns the data of the object.
+     * 
+     * @return T (data)
+     */
+    public T getData() {
+        return this.data;
+    }
+
+    /**
+     * 
+     * This method returns itself to the caller.
+     * 
+     * @return
+     */
+    public GenericNode<T> getNode() {
+        return this;
+    }
+
+    /**
+     * 
+     * This method updates the pointer for the next node.
+     * 
+     * @param nextNode
+     */
+    public void setNextNode(GenericNode<T> nextNode) {
+        this.nextNode = nextNode;
+    }
+
+    /**
+     * 
+     * This method returns the node stored in the next node pointer.
+     * 
+     * @return GenericNode object
+     */
+    public GenericNode<T> getNextNode() {
+        return nextNode;
+    }// end getNextNode
+}
