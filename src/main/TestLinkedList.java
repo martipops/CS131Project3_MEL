c00343608382186c10db06ed5e43fc47c4878c20
martipops <melonnemann@gmail.com>
2023-03-30 14:50:13 -0400

    updated class

--- src/main/TestLinkedList.java
+++ src/main/TestLinkedList.java
@@ -1,27 +1,27 @@
 package main;
+
 import linkedlist.Node;
 import linkedlist.LinkedList;
 
 public class TestLinkedList {
 
 	public static void main(String[] args) {
-		
-		LinkedList myList=new LinkedList();
-		
-		Node aNode=new Node();
+
+		LinkedList myList = new LinkedList();
+
+		Node aNode = new Node();
 		aNode.setData("Element 1");
 		myList.addNode(aNode);
 		aNode = new Node();
 		aNode.setData("Element 2");
 		myList.addNode(aNode);
-		
-		Node tempnode=myList.getList();
-		do 
-		{
+
+		Node tempnode = myList.getList();
+		do {
 			System.out.println(tempnode.getData());
-			tempnode=tempnode.getNextNode();
-		} while (tempnode!=null);
+			tempnode = tempnode.getNextNode();
+		} while (tempnode != null);
 
-	}//end main
+	}
 
-}//end class
+}
